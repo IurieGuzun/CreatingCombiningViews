ea3d3ab7f0f10a9ba12db924c7b58cfe04891723
Iurie Guzun <iurie_guzun@yahoo.com>
2020-08-19 10:22:00 -0400

    Rename to FavoritePlaces

--- FavoritePlaces/CircleImage.swift
+++ FavoritePlaces/CircleImage.swift
@@ -11,8 +11,9 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
-        .clipShape(Circle())
-            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .clipShape(Ellipse())
+            .overlay(Circle()
+            .stroke(Color.white, lineWidth: 4))
         .padding(4)
         .shadow(radius: 10)
        }
